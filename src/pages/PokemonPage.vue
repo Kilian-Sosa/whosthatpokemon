99add41a1da4fa60311d27bee1ea213f4b20d54b
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-04-29 23:07:55 +0100

    feat:  Dynamically display the Pokemon picture

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>Who's that Pokémon?</h1>
-  <PokemonPicture :pokemonId="251" :showPokemon="false"/>
-  <PokemonOptions :pokemons="pokemonIds"/>
+  <h1 v-if="!pokemon">Loading... Please wait</h1>
+  <div v-else>
+    <h1>Who's that Pokémon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="false"/>
+    <PokemonOptions :pokemons="pokemonIds"/>
+  </div>
 </template>
 
 <script>
@@ -14,12 +17,14 @@ export default {
     data() {
         return {
             pokemonIds: [],
-            correctPokemonId: null
+            pokemon: null
         }
     },
     methods: {
       async mixPokemons() {
         this.pokemonIds = await getPokemonOptions();
+
+        this.pokemon = this.pokemonIds[Math.floor(Math.random() * this.pokemonIds.length)];
       }
     },
     mounted() {
